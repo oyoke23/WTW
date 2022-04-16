19d13f534995cd8ebd1e120beebeb488fb4cc160
Ianm22 <infinitomix@gmail.com>
2022-04-16 02:34:49 +0200

    feat: compare book item created

--- app/src/main/java/com/pake/nsqlproject/model/CompareBookAdapter.kt
+++ app/src/main/java/com/pake/nsqlproject/model/CompareBookAdapter.kt
@@ -6,10 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.pake.nsqlproject.R
 import com.pake.nsqlproject.data.Book
+import com.pake.nsqlproject.data.CompareBook
 import com.pake.nsqlproject.databinding.ItemBookBinding
+import com.pake.nsqlproject.databinding.ItemCompareBookBinding
 import com.pake.nsqlproject.ui.comparebooklist.CompareBookListFragment
 
-class CompareBookAdapter(private val bookList: List<Book>, private val clickListener: CompareBookListFragment): RecyclerView.Adapter<CompareBookAdapter.BookHolder>() {
+class CompareBookAdapter(private val bookList: List<CompareBook>, private val clickListener: CompareBookListFragment): RecyclerView.Adapter<CompareBookAdapter.BookHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -23,14 +25,35 @@ class CompareBookAdapter(private val bookList: List<Book>, private val clickList
     override fun getItemCount(): Int = bookList.size
 
     inner class BookHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener, View.OnLongClickListener {
-        private val binding = ItemBookBinding.bind(view)
+        private val binding = ItemCompareBookBinding.bind(view)
 
-        fun render(book: Book) {
-            binding.tvTitle.text = book.name
-            binding.tvStatus.text = book.status
-            binding.tvReadCh.text = book.readCh
-            binding.tvTotalCh.text = book.totalCh
-            binding.tvScore.text = book.score
+        fun render(compareBook: CompareBook) {
+            binding.tvTitle.text = compareBook.book_1?.name
+
+            if (compareBook.book_1 != null) {
+                binding.tvUserScore1.text = compareBook.book_1?.score.toString()
+                binding.tvUserStatus1.text = compareBook.book_1?.status
+                binding.tvUserReadCh1.text = compareBook.book_1?.readCh.toString()
+                binding.tvUserTotalCh1.text = compareBook.book_1?.totalCh.toString()
+            } else {
+                binding.tvUserScore1.text = "-"
+                binding.tvUserStatus1.text = "-"
+                binding.tvUserReadCh1.text = "-"
+                binding.tvUserTotalCh1.text = "-"
+            }
+
+            if (compareBook.book_2 != null) {
+                binding.tvUserScore2.text = compareBook.book_2?.score.toString()
+                binding.tvUserStatus2.text = compareBook.book_2?.status
+                binding.tvUserReadCh2.text = compareBook.book_2?.readCh.toString()
+                binding.tvUserTotalCh2.text = compareBook.book_2?.totalCh.toString()
+            } else {
+                binding.tvUserScore2.text = "-"
+                binding.tvUserStatus2.text = "-"
+                binding.tvUserReadCh2.text = "-"
+                binding.tvUserTotalCh2.text = "-"
+            }
+
         }
 
         init {
