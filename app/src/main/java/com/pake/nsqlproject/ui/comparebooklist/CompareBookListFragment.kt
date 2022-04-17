2cabb9595851b1abd95ec4e0f10b21fff21aab55
Ianm22 <infinitomix@gmail.com>
2022-04-18 01:48:55 +0200

    feat: filter in compare books implemented

--- app/src/main/java/com/pake/nsqlproject/ui/comparebooklist/CompareBookListFragment.kt
+++ app/src/main/java/com/pake/nsqlproject/ui/comparebooklist/CompareBookListFragment.kt
@@ -2,17 +2,19 @@ package com.pake.nsqlproject.ui.comparebooklist
 
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.pake.nsqlproject.R
 import com.pake.nsqlproject.SharedViewModel
 import com.pake.nsqlproject.data.CompareBook
 import com.pake.nsqlproject.databinding.FragmentCompareBookListBinding
 import com.pake.nsqlproject.model.CompareBookAdapter
+import com.pake.nsqlproject.model.ManageData
+import java.util.*
 
 
 class CompareBookListFragment : Fragment(), CompareBookAdapter.OnItemClickListener {
@@ -22,6 +24,8 @@ class CompareBookListFragment : Fragment(), CompareBookAdapter.OnItemClickListen
     private var _binding: FragmentCompareBookListBinding? = null
     private val binding get() = _binding!!
     private lateinit var compareBookAdapter: CompareBookAdapter
+    private var compareBookList: List<CompareBook> = listOf()
+    private var searchMode : Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,11 +33,64 @@ class CompareBookListFragment : Fragment(), CompareBookAdapter.OnItemClickListen
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentCompareBookListBinding.inflate(inflater, container, false)
-        initRecycler(sharedViewModel.compareBookList.value!!)
+        compareBookList = sharedViewModel.compareBookList.value!!
+        setHasOptionsMenu(true)
+        initRecycler()
         return binding.root
     }
 
-    private fun initRecycler(compareBookList: List<CompareBook>) {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_search, menu)
+        val searchItem = menu.findItem(R.id.search_button)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false;
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != "") {
+                    filter(newText!!)
+                }
+                if (newText == "" && searchMode) {
+                    filter(newText)
+                }
+                return true
+            }
+        })
+        searchView.setOnCloseListener{
+            initRecycler()
+            false
+        }
+
+        val expandListener = object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                searchMode = true
+                filter("")
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                searchMode = false
+                initRecycler()
+                return true
+            }
+
+        }
+        searchItem.setOnActionExpandListener(expandListener)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    private fun filter(newText: String){
+        val tempList: List<CompareBook> = compareBookList.filter {
+            it.book_1?.name?.lowercase()?.contains(newText.lowercase()) == true ||
+                    it.book_2?.name?.lowercase()?.contains(newText.lowercase()) == true
+        }
+
+        binding.rvCompareBookList.adapter = CompareBookAdapter(tempList, this)
+    }
+
+    private fun initRecycler() {
         Log.i("Init recycler", "initRecycler")
         binding.rvCompareBookList.layoutManager = LinearLayoutManager(context)
         compareBookAdapter = CompareBookAdapter(compareBookList,this)
