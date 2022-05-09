111a7db63b97b3e708f0f598231062b9047fcfce
oyoke26 <kevincyclops@hotmail.com>
2022-05-09 18:50:41 +0200

    fix: hash code

--- app/src/main/java/com/pake/nsqlproject/ui/createaccount/CreateAccountActivity.kt
+++ app/src/main/java/com/pake/nsqlproject/ui/createaccount/CreateAccountActivity.kt
@@ -7,8 +7,6 @@ import com.pake.nsqlproject.MainActivity
 import com.pake.nsqlproject.data.*
 import com.pake.nsqlproject.databinding.ActivityCreateAccountBinding
 import com.pake.nsqlproject.model.ManageData
-import java.math.BigInteger
-import java.security.MessageDigest
 
 class CreateAccountActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateAccountBinding
@@ -23,11 +21,8 @@ class CreateAccountActivity : AppCompatActivity() {
         val personalList = mutableListOf<PersonalList>()
         val friendsList = mutableListOf<Friend>()
         binding.btnLogin.setOnClickListener {
-            val md5String = Md5(binding.etUsername.text.toString() + binding.etEmail.text.toString())
-
             val alphabet: CharRange = ('0'..'z')
-            var randomString: String = List(30) { alphabet.random() }.joinToString("")
-            randomString = md5String + randomString
+            val randomString: String = List(50) { alphabet.random() }.joinToString("")
             manageData.setData( AllData(
                 PersonalInfo(
                     "-1",
@@ -41,9 +36,5 @@ class CreateAccountActivity : AppCompatActivity() {
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-    }
-    fun Md5(input:String): String {
-        val md = MessageDigest.getInstance("MD5")
-        return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
     }
 }
